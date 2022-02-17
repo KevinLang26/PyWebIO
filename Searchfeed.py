1013b19d56b3aa7ce0a3a14654afad7bcc592d27
KevinLang26 <90727834+KevinLang26@users.noreply.github.com>
2022-02-17 15:16:20 -0800

    Create Searchfeed.py

---
+++ Searchfeed.py
@@ -0,0 +1,18 @@
+from pywebio import start_server
+from pywebio.input import *
+from pywebio.output import *
+from pywebio.session import *
+from pywebio.pin import *
+
+
+from main import put_feed
+
+
+
+def main():
+    put_input('usernames', label='Enter the user(s) you are looking for',
+              placeholder='Multiple users can be entered seperated by ";"')
+    put_buttons(['Search'], lambda _: put_feed(pin.usernames))
+
+
+start_server(main, port=8080, debug=True)
