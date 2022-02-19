191f5a364ce139e27ba27559e67233cb4c313500
KevinLang26 <90727834+KevinLang26@users.noreply.github.com>
2022-02-18 20:06:33 -0800

    Update Subfeed.py

--- Subfeed.py
+++ Subfeed.py
@@ -23,3 +23,29 @@ def main():
             put_sub_feed(a_dict["subscribed"])
 
 start_server(main, port=8080, debug=True)
+
+
+#Build jazzy version
+
+from pywebio.output import *
+import json
+import os
+from Functions import put_sub_feed
+
+lists = ['']
+a_dict = {''}
+list = set()
+
+
+def main():
+    file_exist = os.path.exists('subscriptions.json')
+
+    if not file_exist:
+        put_text("You are not following anyone")
+    else:
+        put_code("Stories submitted by users that I follow")
+        put_text("Feed")
+        with open('subscriptions.json', 'r+') as files:
+            a_dict = json.load(files)
+            put_sub_feed(a_dict["subscribed"])
+
