e4054cad25e9582aa02eb6cd78182222033835c4
KevinLang26 <90727834+KevinLang26@users.noreply.github.com>
2022-02-18 20:05:59 -0800

    Update Searchfeed.py

--- Searchfeed.py
+++ Searchfeed.py
@@ -16,3 +16,23 @@ def main():
 
 
 start_server(main, port=8080, debug=True)
+
+
+
+# Build jazzy app version 
+from pywebio.input import *
+from pywebio.output import *
+from pywebio.session import *
+from pywebio.pin import *
+
+
+from Functions import put_feed
+from Functions import subscribe
+
+
+
+def main():
+    put_input('usernames', label='Enter the user(s) you are looking for',
+              placeholder='Multiple users can be entered seperated by ";"')
+    put_buttons(['Search'], lambda _: put_feed(pin.usernames))
+
